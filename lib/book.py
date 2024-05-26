98de09d3eb29cc7b5391b122da751e1a10bed0bd
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-05-26 10:40:52 +0300

    modified book.py and shoe.py

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+my_book = Book("And Then There Were None", 272)
+
+print(my_book.title)          
+print(my_book.page_count)    
+
+my_book.page_count = "three hundred"
+
+print(my_book.page_count)    
+
+my_book.turn_page()           
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size
+        self._condition = None
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New" 
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @condition.setter
+    def condition(self, value):
+        if isinstance(value, str):
+            self._condition = value
+        else:
+            print("Condition must be a string")
+
